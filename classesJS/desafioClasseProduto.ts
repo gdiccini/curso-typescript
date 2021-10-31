cf07463af8a8006ce4a0928b6ef8ba65a8a3dd02
giovani.diccini <giovani.diccini@gmail.com>
2021-10-31 20:42:28 -0300

    desafio metodos de classe

--- classesJS/desafioClasseProduto.ts
+++ classesJS/desafioClasseProduto.ts
@@ -1,9 +1,17 @@
 class Product {
   constructor(public name: string, public price: number, public discount: number = 0) {}
+
+  public discountedPrice(): number {
+    return this.price * (1 - this.discount)
+  }
+
+  public resume(): string {
+    return `${this.name} custa R$${this.discountedPrice()} (${this.discount * 100}% off)`;
+  }
 }
 
-const product1 = new Product('Meu produto1', 10);
+const product1 = new Product('Meu produto1', 10, 0.1);
 const product2 = new Product('Meu produto2', 30, 0.2);
 
-console.log(product1)
+console.log(product1.resume());
 console.log(product2)
